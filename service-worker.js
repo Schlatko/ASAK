94467c7d1133a17b5a95f8db0b1bf4864ba3f542
saschko <saschaschlotter@me.com>
2024-02-27 12:16:37 +0200

    Updates

--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "/ASAK/precache-manifest.7469e606b3b1346f3976ebb7a79ba900.js"
+  "/ASAK/precache-manifest.ecd24bfdadf6d653c80e9e510ab56603.js"
 );
 
 workbox.clientsClaim();
