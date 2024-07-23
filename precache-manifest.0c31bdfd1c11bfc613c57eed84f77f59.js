b6e877da42cf79924a2334264867aec9aaaebdd3
saschko <saschaschlotter@me.com>
2024-07-23 11:20:13 +0300

    Updates

--- precache-manifest.0c31bdfd1c11bfc613c57eed84f77f59.js
+++ precache-manifest.0c31bdfd1c11bfc613c57eed84f77f59.js
@@ -68,36 +68,40 @@ self.__precacheManifest = [
     "url": "/ASAK/static/media/ionicons.2c2ae068.eot"
   },
   {
-    "revision": "26ec3c7d0366e0825d705c6e224a8803",
-    "url": "/ASAK/static/media/et-line.26ec3c7d.eot"
-  },
-  {
     "revision": "05acfdb568b3df49ad31355b19495d4a",
     "url": "/ASAK/static/media/ionicons.05acfdb5.woff"
-  },
-  {
-    "revision": "c037dbbc0e6790f30e824a50010df5fb",
-    "url": "/ASAK/static/media/ionicons.c037dbbc.svg"
-  },
-  {
-    "revision": "b01ff252761958325faab1535c90c87f",
-    "url": "/ASAK/static/media/et-line.b01ff252.woff"
-  },
-  {
-    "revision": "98126e3e1238b0f3b941ad285320ce28",
-    "url": "/ASAK/static/media/et-line.98126e3e.ttf"
   },
   {
     "revision": "2bea85e74624239934653d10a63c88bc",
     "url": "/ASAK/static/media/et-line.2bea85e7.svg"
   },
   {
+    "revision": "b01ff252761958325faab1535c90c87f",
+    "url": "/ASAK/static/media/et-line.b01ff252.woff"
+  },
+  {
     "revision": "24712f6c47821394fba7942fbb52c3b2",
     "url": "/ASAK/static/media/ionicons.24712f6c.ttf"
   },
   {
+    "revision": "26ec3c7d0366e0825d705c6e224a8803",
+    "url": "/ASAK/static/media/et-line.26ec3c7d.eot"
+  },
+  {
+    "revision": "c037dbbc0e6790f30e824a50010df5fb",
+    "url": "/ASAK/static/media/ionicons.c037dbbc.svg"
+  },
+  {
+    "revision": "98126e3e1238b0f3b941ad285320ce28",
+    "url": "/ASAK/static/media/et-line.98126e3e.ttf"
+  },
+  {
     "revision": "6e682f8dbb59d43f6c6b713894541605",
     "url": "/ASAK/static/media/map-bg.6e682f8d.png"
+  },
+  {
+    "revision": "c83dc045ed2e098d5b8708018ec4286c",
+    "url": "/ASAK/static/media/travel-bg-left.c83dc045.jpg"
   },
   {
     "revision": "c83dc045ed2e098d5b8708018ec4286c",
@@ -108,16 +112,16 @@ self.__precacheManifest = [
     "url": "/ASAK/static/media/travel-bg-right.c83dc045.jpg"
   },
   {
-    "revision": "c83dc045ed2e098d5b8708018ec4286c",
-    "url": "/ASAK/static/media/travel-bg-left.c83dc045.jpg"
+    "revision": "3ed47b8703d0f7bf441f3d5bd877ed24",
+    "url": "/ASAK/static/media/parallax-bg.3ed47b87.jpg"
   },
   {
     "revision": "fe77ad1699e236c9748f9420e8d6a756",
     "url": "/ASAK/static/media/parallax-bg-3.fe77ad16.jpg"
   },
   {
-    "revision": "3ed47b8703d0f7bf441f3d5bd877ed24",
-    "url": "/ASAK/static/media/parallax-bg.3ed47b87.jpg"
+    "revision": "fe77ad1699e236c9748f9420e8d6a756",
+    "url": "/ASAK/static/media/parallax-bg-9.fe77ad16.jpg"
   },
   {
     "revision": "fe77ad1699e236c9748f9420e8d6a756",
@@ -125,15 +129,11 @@ self.__precacheManifest = [
   },
   {
     "revision": "fe77ad1699e236c9748f9420e8d6a756",
-    "url": "/ASAK/static/media/parallax-bg-6.fe77ad16.jpg"
-  },
-  {
-    "revision": "fe77ad1699e236c9748f9420e8d6a756",
     "url": "/ASAK/static/media/parallax-bg-7.fe77ad16.jpg"
   },
   {
     "revision": "fe77ad1699e236c9748f9420e8d6a756",
-    "url": "/ASAK/static/media/parallax-bg-9.fe77ad16.jpg"
+    "url": "/ASAK/static/media/parallax-bg-6.fe77ad16.jpg"
   },
   {
     "revision": "3ed47b8703d0f7bf441f3d5bd877ed24",
@@ -164,24 +164,24 @@ self.__precacheManifest = [
     "url": "/ASAK/static/media/title-hero-1.9eafde0a.jpg"
   },
   {
-    "revision": "9eafde0ac30b657839494322709c5f4c",
-    "url": "/ASAK/static/media/title-hero-2.9eafde0a.jpg"
+    "revision": "ae5b79392de506477eb920fc0b60844e",
+    "url": "/ASAK/static/media/BGContact2.ae5b7939.png"
   },
   {
     "revision": "9eafde0ac30b657839494322709c5f4c",
     "url": "/ASAK/static/media/title-hero-3.9eafde0a.jpg"
   },
   {
-    "revision": "ae5b79392de506477eb920fc0b60844e",
-    "url": "/ASAK/static/media/BGContact2.ae5b7939.png"
-  },
-  {
-    "revision": "7c6a5bd76f11cf6161da7a76b4e050d5",
-    "url": "/ASAK/static/media/title-hero-6.7c6a5bd7.jpg"
+    "revision": "9eafde0ac30b657839494322709c5f4c",
+    "url": "/ASAK/static/media/title-hero-2.9eafde0a.jpg"
   },
   {
     "revision": "7c6a5bd76f11cf6161da7a76b4e050d5",
     "url": "/ASAK/static/media/title-hero-7.7c6a5bd7.jpg"
+  },
+  {
+    "revision": "7c6a5bd76f11cf6161da7a76b4e050d5",
+    "url": "/ASAK/static/media/title-hero-6.7c6a5bd7.jpg"
   },
   {
     "revision": "7c6a5bd76f11cf6161da7a76b4e050d5",
@@ -204,16 +204,16 @@ self.__precacheManifest = [
     "url": "/ASAK/static/media/404.e4301010.gif"
   },
   {
-    "revision": "05760c1f01957d737e0adb4474144009",
-    "url": "/ASAK/static/media/protohead.05760c1f.png"
-  },
-  {
     "revision": "3c76f6c06065b71b355a09641a619be8",
     "url": "/ASAK/static/media/dbfhead.3c76f6c0.png"
   },
   {
     "revision": "627973ca2a47edbdd8978c2ca2d51edb",
     "url": "/ASAK/static/media/laserhead.627973ca.png"
+  },
+  {
+    "revision": "05760c1f01957d737e0adb4474144009",
+    "url": "/ASAK/static/media/protohead.05760c1f.png"
   },
   {
     "revision": "915619332e798fa8aea596ff320dd54d",
