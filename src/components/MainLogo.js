860bbfbc57d6500abc534ea886282a9f4fd7b0a0
saschko <saschaschlotter@me.com>
2026-01-21 13:56:38 +0200

    LogoOnMobile

--- src/components/MainLogo.js
+++ src/components/MainLogo.js
@@ -6,60 +6,50 @@ import React from 'react';
 import { Link } from 'react-router-dom'; // Import für schnelles Navigieren
 import { useTranslation } from 'react-i18next'; // Um die Sprache zu erkennen
 
+
 const MainLogo = ({ fixed }) => {
   const { i18n } = useTranslation();
   const homePath = `/${i18n.language || 'bg'}`;
 
-  // Dynamisches Laden des Logos. Wir nutzen den Pfad, der in deiner Umgebung funktioniert.
-  let logoImg;
+  // Dynamische Ermittlung des Logo-Pfads
+  let finalLogo;
   try {
-    // Versuch 1: Interner Assets-Ordner (Webpack/CRA)
-    logoImg = require("../assets/images/mainLogoAsak.png");
+    finalLogo = require("../assets/images/mainLogoAsak.png");
   } catch (e) {
-    // Versuch 2: Public Ordner (Fallschirm)
-    logoImg = "/assets/images/mainLogoAsak.png";
+    finalLogo = "/assets/images/mainLogoAsak.png";
   }
 
   return (
-    <div className="navbar-header">
-      <div className="logo min-w-[140px] flex items-center h-full">
-        <Link to={homePath} className="navbar-brand block">
-          {/* WICHTIG: Wir entfernen die harten display-none Styles. 
-            Stattdessen nutzen wir opacity oder lassen die Template-CSS Klassen arbeiten.
-          */}
+    <div className="navbar-header flex items-center">
+      {/* !flex und items-center erzwingen die Sichtbarkeit im Bootstrap-Grid */}
+      <Link to={homePath} className="navbar-brand !flex items-center !m-0 !p-0">
           <img 
-            className={`logo-display transition-opacity duration-300 ${fixed ? 'opacity-0 absolute' : 'opacity-100 relative'}`} 
-            src={logoImg} 
+            className={`logo logo-display ${fixed ? 'hidden' : 'block'}`} 
+            src={finalLogo} 
             alt="ASAK Logo" 
             width="140" 
             height="40"
             style={{ 
                 maxWidth: '140px',
                 height: 'auto',
-                objectFit: 'contain',
-                display: 'block'
+                objectFit: 'contain'
             }} 
           />
           <img 
-            className={`logo-scrolled transition-opacity duration-300 ${fixed ? 'opacity-100 relative' : 'opacity-0 absolute'}`} 
-            src={logoImg} 
+            className={`logo logo-scrolled ${fixed ? 'block' : 'hidden'}`} 
+            src={finalLogo} 
             alt="ASAK Logo" 
             width="140" 
             height="40"
             style={{ 
                 maxWidth: '140px',
                 height: 'auto',
-                objectFit: 'contain',
-                display: 'block'
+                objectFit: 'contain'
             }} 
           />
-        </Link>
-      </div>
+      </Link>
     </div>
   );
 };
-
-
-
 
 export default MainLogo;
